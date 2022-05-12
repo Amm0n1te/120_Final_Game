1267c1d2735fac210f39a0a41e4c6c7194c83687
Malachi Maldonado <mmmaldon@ucsc.edu>
2022-05-11 19:14:23 -0700

    black screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log("hi");
+let config = {
+    type: Phaser.CANVAS,
+    width: 960,
+    height: 720,
+    autoCenter: true,
+    scene: [Title,],
+};
+
+let game = new Phaser.Game(config);
