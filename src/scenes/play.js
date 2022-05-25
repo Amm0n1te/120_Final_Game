c5c6bdd3331d4def43fa6ff553eadfd7d1885b29
Malachi Maldonado <mmmaldon@ucsc.edu>
2022-05-25 14:27:03 -0700

    replaced eye texture

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -18,8 +18,8 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0, 960, 720, 'background').setOrigin(0, 0);
         this.mist = this.add.tileSprite(0, 0, 960, 720, 'mist').setOrigin(0, 0);
         this.hands = this.add.sprite(0, 0, 'hands').setOrigin(0, 0);
-        this.cairn = this.add.sprite(520, 390, 'cairn').setOrigin(0, 0);
-        this.cairn.setScale(0.2);
+        this.cairn = this.add.sprite(520, 500, 'cairn').setOrigin(0, 0);
+        this.cairn.setScale(1.2);
         this.hugh = new Player(this, 290, 445, 'hugh', 0).setOrigin(0,0);
         this.eye = new Eye(this, 655, 11, 'eyeDown').setOrigin(0,0);
         this.door = this.add.sprite(game.config.width, 250, 'door').setOrigin(1, 0);
